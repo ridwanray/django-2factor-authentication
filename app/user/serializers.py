c5973c604aec51028ad7cf200c5857ac6790af2a
ridwanray <alabarise@gmail.com>
2023-06-20 07:53:05 +0100

    resolve migrations

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,10 +1,10 @@
 
-from django.contrib.auth import authenticate
 from django.utils.translation import gettext_lazy as _
-from rest_framework import exceptions, serializers
+from rest_framework import exceptions
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from .models import User
+
 
 class CustomObtainTokenPairSerializer(TokenObtainPairSerializer):
 
@@ -27,28 +27,6 @@ class CustomObtainTokenPairSerializer(TokenObtainPairSerializer):
         token['firstname'] = user.firstname
         token['lastname'] = user.lastname
         token["email"] = user.email
-        token["roles"] = user.roles
         return token
 
-
-class AuthTokenSerializer(serializers.Serializer):
-    """Serializer for user authentication object"""
-
-    email = serializers.CharField()
-    password = serializers.CharField(
-        style={"input_type": "password"}, trim_whitespace=False)
-
-    def validate(self, attrs):
-        """Validate and authenticate the user"""
-        email = attrs.get("email")
-        password = attrs.get("password")
-        if email:
-            user = authenticate(request=self.context.get(
-                "request"), username=email.lower().strip(), password=password)
-
-        if not user:
-            msg = _("Unable to authenticate with provided credentials")
-            raise serializers.ValidationError(msg, code="authentication")
-        attrs["user"] = user
-        return attrs
 
--- docker/prod/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-python manage.py makemigrations --no-input
-python manage.py migrate --no-input
-rm celerybeat.pid
-rm logs/debug.log
-exec "$@"
